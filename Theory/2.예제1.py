88a4717b2c640d7b441b999b239a1cce5ff1f479
CodeEunbi <eunbi801@naver.com>
2025-01-31 16:14:47 +0900

    no api key

--- Theory/2.예제1.py
+++ Theory/2.예제1.py
@@ -1,11 +1,14 @@
 import requests
 
-#서울위 위도
+#서울의 위도
 lat = 37.56
 #서울의 경도
 lon = 126.97
-API_key = '776c53f8b46f57dd9223b306b4cbaa9d'
+API_key = ''
 #API 요청 URL
 URL = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}'
 data = requests.get(URL).json()
-print(data)
+#key값 출력
+print(data.keys())
+
+#데이터 추출 - 원하는 값만 추출
